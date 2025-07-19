d0175a56d610ee44972af23df63f56202ba9a628
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-19 03:54:21 +0000

    Updating and testing args (successfully)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Configuring JDK in version " + args[0]);
+        System.out.println("Programming in " + args[1]);
     }
 }
