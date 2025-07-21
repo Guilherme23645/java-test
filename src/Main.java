c5761dd48f662240b282c4c21938c510b2ce765d
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-21 17:17:34 +0000

    remove input reading, add operators test

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-
-    private final static String WELCOME = "What is your name?";
     public static void main(String[] args) throws Exception {
-        // Reading input
-        var scanner = new Scanner(System.in);
-        System.out.println(WELCOME);
-        var x = scanner.next();
-        System.out.printf("Hi, %s!%n",x);
-        scanner.close();
+        // Dealing with attribute and logic operators
     }
 }
