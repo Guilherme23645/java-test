4acd6b6ccdc24dbac9a287b151e8ed83069e90bd
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-21 00:38:18 +0000

    update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+
+    private final static String WELCOME = "What is your name?";
     public static void main(String[] args) {
         // Reading input
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name? ");
+        System.out.println(WELCOME);
         String x = scanner.next();
         System.out.printf("Hi, %s!%n",x);
         scanner.close();
