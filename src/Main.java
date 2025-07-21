8189c92ef75156b041551599b8cc582b0e6dd1c7
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-21 18:38:51 +0000

    arithmetic operators

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Dealing with attribute and logic operators
-        var scanner = new Scanner(System.in);
-        System.out.println("Quantos anos você tem?");
-        var age = scanner.nextInt();
-        var canDrive = age > 17;
-        System.out.printf("Você pode dirigir? (%s)\n", canDrive);
-        scanner.close();
+        // Dealing with arithmetic operators
     }
 }
