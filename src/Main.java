402deef0fecbdf4b384649e3c29ba5c58a84dd76
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-21 21:09:14 +0000

    bitwise operators

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Dealing with arithmetic operators
-        var scanner = new Scanner(System.in);
-        System.out.println("First number: ");
-        var a = scanner.nextInt();
-        System.out.println("Second number:");
-        var b = scanner.nextInt();
-        System.out.printf("%s + %s = %s%n",a,b,a+b);
-        System.out.println("Third number:");
-        var c = scanner.nextInt();
-        System.out.printf("Square root of %s is %s%n", c, Math.sqrt(c));
-        System.out.println("Fourth number: ");
-        var d = scanner.nextInt();
-        System.out.println("Fifth number:");
-        var e = scanner.nextInt();
-        System.out.printf("%s to the power of %s is %s%n",d,e,Math.pow(d,e));
-        scanner.close();
+        // Dealing with bitwise operators
     }
 }
