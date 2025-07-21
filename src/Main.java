dc60c948f641394310ef3a527c4de7aa2041f3b6
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-21 19:09:20 +0000

    add arithmetic test

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Dealing with arithmetic operators
+        var scanner = new Scanner(System.in);
+        System.out.println("First number: ");
+        var a = scanner.nextInt();
+        System.out.println("Second number:");
+        var b = scanner.nextInt();
+        System.out.printf("%s + %s = %s",a,b,a+b);
+        scanner.close();
     }
 }
