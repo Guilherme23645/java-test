17c27794d9fabb5063b7187cbd745d1777d6e212
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-21 17:19:38 +0000

    added test

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Dealing with attribute and logic operators
+        var scanner = new Scanner(System.in);
+        System.out.println("Quanto e 2+2?");
+        var result = scanner.nextInt();
+        var isRight = result == 4;
+        System.out.printf("O resultado e 4. Voce acertou? (%s)", isRight);
+        scanner.close();
     }
 }
