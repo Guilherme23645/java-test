e5451070fe19a0d444f128fcf563007e5c8d766a
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-24 01:16:23 +0000

    update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         // Dealing with bitwise operators
+        var a = 6;
+        var binA = Integer.toBinaryString(a);
+        System.out.printf("Number %s has %s as its binary representation.",a,binA);
     }
 }
