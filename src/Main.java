1be0b6937393c6a8922269f17e68f6ac793fa0c2
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-29 20:23:19 +0000

    feat: turned Person class into record

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Dealing with objects
-        Person.setTest("test");
-        var male = new Person("Robert", 18);
-        var female = new Person("Alice",19);
-        System.out.printf("Name: %s, Age: %s%n",male.getName(),male.getAge());
-        System.out.printf("Name: %s, Age: %s%n",female.getName(),female.getAge());
-        System.out.printf("Static variable: %s%n",Person.getTest());
+        // Dealing with records
+        var person = new Person("Alice");
+        System.out.println(person);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,26 +1,3 @@
-public class Person {
-    private String name;
-    private int age;
-    private static String test;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public static void setTest(String testValue) {
-        test = testValue;
-    }
-
-    public static String getTest() {
-        return test;
-    }
+public record Person(String name) {
+    
 }
