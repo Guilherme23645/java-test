80818435dc10e93e1c8f1a89a098bb2e343a29e9
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-29 02:12:52 +0000

    displaying them

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         var female = new Person();
         female.name = "Alice";
         female.age = 19;
+        System.out.printf("Name: %s, Age: %s",male.name,male.age);
+        System.out.printf("Name: %s, Age: 5s",female.name,female.age);
     }
 }
