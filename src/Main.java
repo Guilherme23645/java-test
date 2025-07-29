e4ea2d760b8288ae715231097d43ff732b512525
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-29 19:59:33 +0000

    removing setters and adding constructor for simplification

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Dealing with objects
         Person.setTest("test");
-        var male = new Person();
-        male.setName("Robert");
-        male.setAge(18);
-        var female = new Person();
-        female.setName("Alice");
-        female.setAge(19);
+        var male = new Person("Robert", 18);
+        var female = new Person("Alice",19);
         System.out.printf("Name: %s, Age: %s%n",male.getName(),male.getAge());
         System.out.printf("Name: %s, Age: %s%n",female.getName(),female.getAge());
         System.out.printf("Static variable: %s%n",Person.getTest());
