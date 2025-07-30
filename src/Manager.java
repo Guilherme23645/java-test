d17ceb4204bd5e61ecfd35c75dc6aaf1ada4fb93
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-30 16:11:43 +0000

    feat: add class properties and methods to Manager class

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,29 @@
-public class Manager {
-    
+public class Manager extends Employee {
+    private String login;
+    private String password;
+    private double commission;
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getCommission() {
+        return this.commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
 }
