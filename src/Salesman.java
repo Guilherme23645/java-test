7315589e59e3b05dfc280de9b358d3a643639fad
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 18:41:26 +0000

    feat: add Salesman class

---
+++ src/Salesman.java
@@ -0,0 +1,11 @@
+public final class Salesman extends Employee {
+    private double percentagePerSale;
+
+    public double getPercentage() {
+        return this.percentagePerSale;
+    }
+
+    public void setPercentage(double percentagePerSale) {
+        this.percentagePerSale = percentagePerSale;
+    }
+}
