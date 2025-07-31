0b852ef27cb2b986dcc1b7ae7787d1ba5962a7f3
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 18:45:44 +0000

    feat: configuring info display

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Dealing with records
-        var person = new Person("Alice",19);
-        System.out.println(person);
-        System.out.println(person.getInfo());
+        // Dealing with inheritance and polymorphism
+        printEmployee(new Manager());
+    }
+
+    public static void printEmployee(Employee employee) {
+        employee.setName("Richard");
+        ((Manager)employee).setLogin("richard");
+        ((Manager)employee).setPassword("1dA@43");
+
+        System.out.println(employee.getClass());
+        System.out.println(employee.getName());
+        System.out.println(((Manager)employee).getLogin());
+        System.out.println(((Manager)employee).getPassword());
     }
 }
