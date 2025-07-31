aa508ea7157aa7549889117e8ff39ed5c98efd92
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 20:30:02 +0000

    feat: add sold amount to Salesman

--- src/Salesman.java
+++ src/Salesman.java
@@ -1,9 +1,11 @@
 public final class Salesman extends Employee {
     private double percentagePerSale;
+    private double soldAmount;
 
-    public Salesman (String code,String name,String address,int age, int salary, double percentagePerSale) {
+    public Salesman (String code,String name,String address,int age, int salary, double percentagePerSale, double soldAmount) {
         super(code,name,address,age,salary);
         this.percentagePerSale = percentagePerSale;
+        this.soldAmount = soldAmount;
     }
 
     public double getPercentage() {
@@ -17,5 +19,17 @@ public final class Salesman extends Employee {
     @Override
     public String getCode() {
         return "SL"+super.getCode();
+    }
+
+    public double getSoldAmount() {
+        return this.soldAmount;
+    }
+
+    public void setSoldAmount(double soldAmount) {
+        this.soldAmount = soldAmount;
+    }
+
+    public double getFullSalary() {
+        return super.getSalary() + (this.soldAmount * this.percentagePerSale) / 100;
     }
 }
