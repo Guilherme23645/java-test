3423c044d267f714447150f8ecbbfd29dbdd660b
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-31 19:12:51 +0000

    feat: further configuring display

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,38 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Dealing with inheritance and polymorphism
         printEmployee(new Manager());
+        printEmployee(new Salesman());
     }
 
     public static void printEmployee(Employee employee) {
-        employee.setName("Richard");
-        ((Manager)employee).setLogin("richard");
-        ((Manager)employee).setPassword("1dA@43");
+        System.out.printf("=======%s=======%n",employee.getClass().getCanonicalName());
+        switch (employee) {
+            case Manager manager -> {
+                manager.setCode("456");
+                manager.setName("Richard");
+                manager.setSalary(2349);
+                manager.setLogin("richard");
+                manager.setPassword("1dA@43");
+                manager.setCommission(1200);
 
-        System.out.println(employee.getClass());
-        System.out.println(employee.getName());
-        System.out.println(((Manager)employee).getLogin());
-        System.out.println(((Manager)employee).getPassword());
+                System.out.println(manager.getCode());
+                System.out.println(manager.getSalary());
+                System.out.println(manager.getName());
+                System.out.println(manager.getLogin());
+                System.out.println(manager.getPassword());
+                System.out.println(manager.getCommission());
+            }
+            case Salesman salesman -> {
+                salesman.setCode("789");
+                salesman.setName("Gabriel");
+                salesman.setSalary(1758);
+                salesman.setPercentage(20);
+
+                System.out.println(salesman.getCode());
+                System.out.println(salesman.getSalary());
+                System.out.println(salesman.getName());
+                System.out.println(salesman.getPercentage());
+            }
+        }
     }
 }
