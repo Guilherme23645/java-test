920cbe8223c974416a7a3d3b455ac5221fc913f8
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-01 09:05:47 +0000

    feat: formatting user display

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> users = List.of(
+            new User("Alice",19),
+            new User("Charlie",18),
+            new User("Pedro",24),
+            new User("Elizabeth",34)
+        );
+
+        users.forEach((User user) -> {
+            System.out.printf("Name: %s, Age: %s%n", user.name(),user.age());
+        });
+    }
+}
