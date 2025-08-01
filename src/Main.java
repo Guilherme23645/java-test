40a3ce6a9fa805f2582d2051a883cc254d951544
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-01 09:57:34 +0000

    feat: add structure for new lesson

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
-import java.util.List;
-import java.util.function.Function;
-
 public class Main {
     public static void main(String[] args) {
-        List<User> users = List.of(
-            new User("Alice",19),
-            new User("Charlie",18),
-            new User("Pedro",24),
-            new User("Elizabeth",34)
-        );
-
-        printStringValue(User::toString, users);
-    }
-
-    private static void printStringValue(Function<User, String> callback, List<User> users) {
-        users.forEach(u -> System.out.println(callback.apply(u)));
+        
     }
 }
